58fc75e6aa43ed3163c8b6dba19468b89fa5b82c
Matthew Moor <meccep.98.98@mail.ru>
2018-11-24 11:52:25 +0300

    "This my first project only HTML5 and CSS3"

---
+++ First_project/js/common.js
@@ -0,0 +1,79 @@
+  $('.sl').slick();
+  $('.sl').
+  $(document).ready(function() {
+  	$('.fa-bars').click(function() {
+  		$('.menuTog').sliderToogle(500);
+
+  	});
+
+  });
+
+$('.super_menu').sliderToogle(500);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+// 	$('.next').click(function(){
+// 		var currentImage = $('.emg.curry');
+// 		var currentImageIndex = $('.img.curry').index();
+// 		var nextImageIndex = currentImageIndex + 1;
+// 		var nextImage = $('.img').eq(nextImageIndex);
+// 		currentImage.fadeOut(1000);
+// 		currentImage.removeClass('curry');
+// 		if(nextImageIndex == ($('.img:last').index()+1)){
+//            $('.img').eq(0).fadeIn(1000);
+//            $('.img').eq(0).addClass('curry');
+//           }  else {
+//            	nextImage.fadeIn(1000);
+//            	nextImage.addClass('curry');
+//            }
+// 	     });
+//      $('.prev').click(function(){
+//      	var currentImage = $('.emg.curry');
+// 		var currentImageIndex = $('.img.curry').index();
+// 		var prevImageIndex = currentImageIndex - 1;
+// 		var prevImage = $('.img').eq(nextImageIndex);
+
+//         currentImage.fadeOut(1000);
+//         currentImage.removeClass('curry');
+//         previmage.addClass('curry');
+//         })
+// });
+  // $('.sl').slick();
