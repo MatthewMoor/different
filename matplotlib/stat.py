fdd473bfb4d548758a9136bc20fe04ad63eb6dd4
Matthew Moor <meccep.98.98@mail.ru>
2019-02-11 00:39:25 +0300

    del

--- matplotlib/stat.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-my_stat = [164000, 41000, 80000, 13000, 27500, 9500]
-
-
-my_data = ['Eugene', 'Audar', 'Artem', 'Aynur', 'Marat', 'Andrew']
-        
-plt.plot(my_data, my_stat)
-plt.show()
